78fd0a0a6e0e2f326695c5169c3859fed51f1c4a
whyimiliving <muhammetemn.yldz@gmail.com>
2025-04-12 07:25:32 +0300

    a

---
+++ The Game/Assets/Scripts/Global FSM/Actions/ActionRestoreMainMusic.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FSM.Actions
+{
+    [CreateAssetMenu(menuName = "FSM/Actions/Restore Main Music")]
+    public class ActionRestoreMainMusic : FSMAction
+    {
+        public float fadeTime = 0.5f;
+
+        public override void Act(FSMContext context)
+        {
+            SoundManager.Instance.RestoreMainMusic(fadeTime);
+        }
+    }
+}
