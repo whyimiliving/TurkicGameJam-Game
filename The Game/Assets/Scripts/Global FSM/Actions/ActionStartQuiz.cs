1dea6d0eb667e4a629089dd102b013cbb0d21315
whyimiliving <muhammetemn.yldz@gmail.com>
2025-04-12 05:58:11 +0300

    player

---
+++ The Game/Assets/Scripts/Global FSM/Actions/ActionStartQuiz.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FSM.Actions
+{
+    [CreateAssetMenu(menuName = "FSM/Actions/Start Dialog Quiz")]
+    public class ActionStartQuiz : FSMAction
+    {
+        public override void Act(FSMContext context)
+        {
+            if (!DialogQuizManager.Instance.QuizInProgress)
+            {
+                DialogQuizManager.Instance.StartQuiz();
+            }
+        }
+    }
+}
---
+++ The Game/Assets/Scripts/Global FSM/Decisions/Quiz/DecisionQuizPassed.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FSM.Decisions
+{
+    [CreateAssetMenu(menuName = "FSM/Decisions/Quiz Passed")]
+    public class DecisionQuizPassed : FSMDecision
+    {
+        public override bool Decide(FSMContext context)
+        {
+           // Debug.Log($"[FSM] QuizPassed: {DialogQuizManager.Instance.QuizPassed}, InProgress: {DialogQuizManager.Instance.QuizInProgress}");
+          //  Debug.Log("Dogru" + DialogQuizManager.Instance.correctCount);
+            return DialogQuizManager.Instance.QuizInProgress &&
+                   DialogQuizManager.Instance.QuizPassed;
+        }
+    }
+}
