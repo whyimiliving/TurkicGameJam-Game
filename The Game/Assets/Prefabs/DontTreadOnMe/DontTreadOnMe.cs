75f0a4b1503aaf9bd00b92f2578a25bd35b78ca8
Goktug <69222008+goktug5000@users.noreply.github.com>
2025-04-13 15:50:56 +0300

    first refactor

--- The Game/Assets/Prefabs/DontTreadOnMe/DontTreadOnMe.cs
+++ The Game/Assets/Prefabs/DontTreadOnMe/DontTreadOnMe.cs
@@ -2,13 +2,34 @@ using UnityEngine;
 
 public class DontTreadOnMe : MonoBehaviour
 {
-    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    public GameObject sfx;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            animator.SetTrigger("interact");
+            if (sfx != null)
+            {
+                Instantiate(sfx, collision.transform);
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spriteRenderer.sprite = sprites[1];
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spriteRenderer.sprite = sprites[0];
         }
     }
 }
